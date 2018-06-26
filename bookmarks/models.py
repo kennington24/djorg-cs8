8017872ac47061ec3734f2e8cb871416aa0c1aae
Kenny <khpham24@gmail.com>
2018-06-26 16:34:32 -0600

    caught up with lecture and added the functionality to bookmarks

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Bookmark(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   name = models.CharField(max_length=200)
-  url = models.TextField(validators=[URLValidator()])
+  url = models.TextField(max_length=200)
   created_at = models.DateTimeField
+
+class PersonalBookmark(Bookmark):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
