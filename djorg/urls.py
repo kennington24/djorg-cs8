48df7653d5af0464f4060fbd745eb3775e836dbe
Kenny <khpham24@gmail.com>
2018-06-29 13:33:54 -0600

    url update

--- djorg/urls.py
+++ djorg/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     re_path(r'^api-token-auth/', views.obtain_auth_token),
     path('admin/', admin.site.urls),
     path(r'api/', include(router.urls)),
+    path(r'/', include(router.urls)),
 ]
