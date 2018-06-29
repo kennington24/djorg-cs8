0f7d3307166bfbbfabc797124c24ae7149a7baea
Kenny <khpham24@gmail.com>
2018-06-29 12:10:16 -0600

    asl;fkd

--- djorg/settings.py
+++ djorg/settings.py
@@ -86,7 +86,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        dj_database_url.config(conn_max_age=600),
     }
 }
 
