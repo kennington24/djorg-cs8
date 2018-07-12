b834a0000f6f10bb5c917e85a1a53641e919d30d
Kenny <khpham24@gmail.com>
2018-07-12 16:49:27 -0600

    make it bettera

--- djorg/settings.py
+++ djorg/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com', 'guarded-headland-77742.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com', 'guarded-headland-77742.herokuapp.com']
 
 # Application definition
 
